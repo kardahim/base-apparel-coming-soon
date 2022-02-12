eb0aa0963c9db6f61e6e105fd5b1ac9608956f4e
Paweł Ciszewski <kardahim@gmail.com>
2022-02-12 14:03:46 +0100

    add event listener

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-function validation() {
+// event listener
+document.getElementById("form").addEventListener("submit", validation);
+
+function validation(event) {
     var data = document.getElementById("email").value;
     let pattern = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
     let result = pattern.exec(data);
@@ -19,6 +22,7 @@ function validation() {
         document.getElementById("email").style.border = "1px solid hsl(0, 93%, 68%)";
         document.getElementById("error").style.visibility = "visible";
         document.getElementById("error-message").innerHTML = message;
-        return false;
+        // if invalid data dont submit
+        event.preventDefault();
     }
 }
